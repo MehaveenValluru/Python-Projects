c704caf63f81e12bfd4714928b6be7c0b0ab9f89
MehaveenValluru <138682657+MehaveenValluru@users.noreply.github.com>
2023-07-15 23:47:09 +0530

    Update PyWhatkit.py

--- PyWhatkit.py
+++ PyWhatkit.py
@@ -1,7 +1,7 @@
 #code for sending message:
 import pywhatkit as py
 txt=str(input("enter the text : "))
-py.sendwhatmsg("+918008653657",txt,14,10)
+py.sendwhatmsg("+919398483068",txt,14,10)
 #code for seaching in youtube:
 py.playonyt("steve cutts")
 #code for seaching a text:
